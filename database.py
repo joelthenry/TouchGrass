d49dde739b3de31080c8bc399acb833739b9c866
Eyal Lahat <eyalahat@gmail.com>
2025-03-01 16:22:56 -0700

    Update database.py
    Bounds checked

--- database.py
+++ database.py
@@ -2,29 +2,33 @@ import mysql.connector
 
 
 def add_user(name: str, email: str, password: str):
-    query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
-    values = (name, email, password)
-    cursor.execute(query, values)
-    connection.commit()
+    db_config = {
+        "host": "Dell_Eyal",
+        "user": "eyalahat",
+        "password": "Eyallahat1??",
+        "database": "flower_users"
+    }
 
+    # connect to mySQL server
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
 
-db_config = {
-    "host": "Dell_Eyal",
-    "user": "eyalahat",
-    "password": "Eyallahat1??",
-    "database": "flower_users"
-}
+    except mysql.connector.Error as err:
+        print("Error in connection to SQL Server \n {err}")
 
-connection = mysql.connector.connect(**db_config)
-cursor = connection.cursor()
-
-
-def main():
-    add_user("Curtis Predum", "curtis.predom@gmail.com", "CUh11")
     cursor.execute("USE flower_users")
+
+    # insert user into database
+    try:
+        query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+        values = (name, email, password)
+        cursor.execute(query, values)
+        connection.commit()
+
+    except:
+        print("Error, can't add user to database.")
+
     cursor.close()
     connection.close()
 
-
-if __name__ == "__main__":
-    main()
