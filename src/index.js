29fdbbd313e999e64c78bb3c6b1a5e2b43243a67
EyaLahaT <eyalahat@gmail.com>
2025-03-02 03:53:07 -0700

    Added function for pins

--- src/index.js
+++ src/index.js
@@ -119,6 +119,16 @@ const requireLogin = (req, res, next) => {
     next();
 };
 
+app.get("/getPins", async (req, res) => {
+    try {
+        const result = await db.any("SELECT latitude, longitude FROM posts"); // Query DB
+        res.json(result); // Send JSON response
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 app.get('/welcome', (req, res) => {
     res.json({ status: 'success', message: 'Welcome!' });
 });
@@ -138,6 +148,7 @@ app.get('/identify', (req, res) => {
 
 app.get('/explore', (req, res) => {
     res.render('pages/map');
+
 });
 
 
